7e8cd4b2e14a7203c1dd19c64a37394916aaf378
Brandonchia <haojin_jia@126.com>
2016-01-24 15:57:02 +0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,16 @@
+data <- read.table("household_power_consumption.txt",head=TRUE,sep=";",na.string ="?")
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+start <- as.Date("2007/02/01",format="%Y/%m/%d")
+end <- as.Date("2007/02/02",format="%Y/%m/%d")
+data1 <- subset(data,data$Date >= start & data$Date <= end)
+mergeDT <- paste(data1$Date,data1$Time)
+convertDT<-strptime(mergeDT, format="%Y-%m-%d %H:%M:%S")
+data1$Datime <- convertDT
+# plot 3
+plot(data1$Datime,data1$Sub_metering_1,type="n",xlab="Time",ylab="Energy Sub Metering")
+lines(data1$Datime,data1$Sub_metering_1,col="black")
+lines(data1$Datime,data1$Sub_metering_2,col="green")
+lines(data1$Datime,data1$Sub_metering_3,col="blue")
+legend("topright",col="black",legend="sub_metering_1")
+dev.copy(png,file="plot3.png")
+dev.off()
