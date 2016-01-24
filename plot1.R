b5385baa86474eed1d91d7b552206cde17cc510c
Brandonchia <haojin_jia@126.com>
2016-01-24 15:55:54 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,13 @@
+# read the dataset
+data <- read.table("household_power_consumption.txt",head=TRUE,sep=";",na.string ="?")
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+start <- as.Date("2007/02/01",format="%Y/%m/%d")
+end <- as.Date("2007/02/02",format="%Y/%m/%d")
+data1 <- subset(data,data$Date >= start & data$Date <= end)
+mergeDT <- paste(data1$Date,data1$Time)
+convertDT<-strptime(mergeDT, format="%Y-%m-%d %H:%M:%S")
+data1$Datime <- convertDT
+# plot 1
+hist(data1$Global_active_power,xlab="Global Active Power(kilowatts)",col="red",main="Global Active Power")
+dev.copy(png,file="plot1.png")
+dev.off()
